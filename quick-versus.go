6069c2c0ccd527fb59c28021dc3534f874442432
neolao <contact@neolao.com>
2022-08-15 16:11:46 +0200

    up

--- quick-versus.go
+++ quick-versus.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "os/exec"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
@@ -9,6 +10,15 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("Hello World")
 
-	w.SetContent(widget.NewLabel("Hello World!"))
+  content := widget.NewButton("click me", func() {
+    cmd := exec.Command("mugen.exe", "-p1", "Street Fighter/Ryu (CVS)/Ryu.def", "-p2", "Street Fighter/Dhalsim (CVS2)/Dhalsim.def")
+    _, err := cmd.CombinedOutput()
+    if err != nil {
+      panic(err)
+    }
+	})
+
+  w.SetFullScreen(true)
+	w.SetContent(content)
 	w.ShowAndRun()
 }
